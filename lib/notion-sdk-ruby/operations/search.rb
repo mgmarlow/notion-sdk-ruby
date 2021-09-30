fab79f67428b87f1e4318e06babc9c2adbf0c7c7
Graham Marlow <mgmarlow@gmail.com>
2021-09-29 17:37:49 -0700

    Add search doc comments

--- lib/notion-sdk-ruby/operations/search.rb
+++ lib/notion-sdk-ruby/operations/search.rb
@@ -1,8 +1,8 @@
 module Notion
   module Operations
     module Search
-      include RequestClient
-
+      # @param [Hash] body
+      # @return [Array]
       def search(body)
         response = post("/v1/search", body.to_json)
         List.new(response.body)
