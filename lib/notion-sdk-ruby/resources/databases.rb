c5e86875afa6a80e41a0b6279019ccaf894b5b6a
Graham Marlow <mgmarlow@gmail.com>
2021-10-02 10:37:06 -0700

    Database docs

--- lib/notion-sdk-ruby/resources/databases.rb
+++ lib/notion-sdk-ruby/resources/databases.rb
@@ -2,28 +2,39 @@ module Notion
   class Databases
     include RequestClient
 
+    # @param [String] database_id
+    # @return [Notion::Database]
     def retrieve(id)
       response = get("/v1/databases/#{id}")
       Database.new(response.body)
     end
 
-    # DEPRECATED
+    # @deprecated
+    # @return [Array<Notion::Database>]
     def list
       warn "DEPRECATED: client.databases.list is deprecated."
       response = get("/v1/databases")
       List.new(response.body)
     end
 
+    # @param [String] database_id
+    # @param [Hash] body
+    # @return [Array<Notion::Database>]
     def query(id, body)
       response = post("/v1/databases/#{id}/query", body.to_json)
       List.new(response.body)
     end
 
+    # @param [Hash] body
+    # @return [Notion::Database]
     def create(body)
       response = post("/v1/databases", body.to_json)
       Database.new(response.body)
     end
 
+    # @param [String] database_id
+    # @param [Hash] body
+    # @return [Notion::Database]
     def update(id, body)
       response = patch("/v1/databases/#{id}", body.to_json)
       Database.new(response.body)
