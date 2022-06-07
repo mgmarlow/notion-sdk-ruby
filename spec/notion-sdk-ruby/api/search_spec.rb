81541e392bd7ee2d5e7a5ba77447cf12f3de9dde
Graham Marlow <mgmarlow@hey.com>
2022-06-06 20:18:55 -0700

    Rewrite search specs with VCR

--- spec/notion-sdk-ruby/api/search_spec.rb
+++ spec/notion-sdk-ruby/api/search_spec.rb
@@ -1,20 +1,15 @@
-RSpec.describe "search" do
-  let(:access_token) { "foo-1234" }
+RSpec.describe "search", :vcr do
+  let(:access_token) { ENV["NOTION_API_KEY"] }
+
   subject(:client) { Notion::Client.new(token: access_token) }
-
-  let(:search_fixture) { load_fixture("search/200_search") }
-
-  before do
-    stub_request(:post, "https://api.notion.com/v1/search")
-      .to_return(body: search_fixture)
-  end
 
   describe "#search" do
     let(:body) do
       {
         query: "External tasks",
         sort: {
-          direction: "ascending"
+          direction: "ascending",
+          timestamp: "last_edited_time"
         }
       }
     end
@@ -25,15 +20,6 @@ RSpec.describe "search" do
       expect(a_request(:post, "https://api.notion.com/v1/search")
         .with(body: body))
         .to have_been_made.once
-    end
-
-    it "should match fixture response" do
-      items = client.search(body).data.map(&:to_h)
-      expected = JSON.parse(search_fixture)["results"]
-
-      items.each_with_index do |item, i|
-        expect(item.to_json).to eq(expected[i].to_json)
-      end
     end
   end
 end
